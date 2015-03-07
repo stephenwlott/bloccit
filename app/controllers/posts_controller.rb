e8473c719c31046c150e5d68c9029468d70bc0e0
Stephen Lott <stephenwlott@gmail.com>
2015-03-07 06:59:55 +0000

    Interlude

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(post_params)
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
   
   def update
     @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:id])
+    @post = current_user.posts.build(post_params)
     authorize @post
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
@@ -40,5 +40,11 @@ class PostsController < ApplicationController
       flash[:error] = "There was an error saving the post. Please try again."
       render :edit
     end
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
