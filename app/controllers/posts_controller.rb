934bc6815acf106bd94e247d23bd7097b715dd46
Stephen Lott <stephenwlott@gmail.com>
2015-03-09 17:03:22 +0000

    Post Markdown Assignment Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,10 +14,12 @@ class PostsController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(post_params)
+    @post.topic = @topic
+    
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to @topic
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
@@ -36,7 +38,7 @@ class PostsController < ApplicationController
     authorize @post
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to @topic
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :edit
