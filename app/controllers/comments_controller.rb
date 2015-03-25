58d1f9f2f7beaaab839c31cb6f7418a08d6115f7
Stephen Lott <stephenwlott@gmail.com>
2015-03-25 16:34:17 +0000

    comments assignment complete 2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    if current_user == nil
+    if current_user.nil?
       flash[:error] = "Log in first if you want to create a comment"
       redirect_to '/users/sign_in'      
     else
-      puts "--- current_user = #{current_user} ---"
-      @post_id = params[:post_id]
       @post = Post.find(params[:post_id])
       @user = current_user
-        @comments = current_user.comments.create(:post_id => @post_id, :user_id => @user.id, :body => comment_params[:body])
+      @comments = current_user.comments.create(:post_id => @post.id, :user_id => @user.id, :body => comment_params[:body])
       if @comments.save
         flash[:notice] = "Comment was saved."
         redirect_to url_for(:action=>"show", :controller=>"posts", :format=>"26", :id=>@post.id , :topic_id=>@post.topic_id)
