d03dc19144e5534c6c0129a1a5660cec6c5bd7d1
Stephen Lott <stephenwlott@gmail.com>
2015-03-31 23:39:03 +0000

    merged destroy assignment

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,18 @@ Rails.application.routes.draw do
   resources :users, only: [:update]
   
   resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments, only: [:create, :destroy]
-      
-      #post 'upvote', on: :member
-      #post 'downvote', on: :member
-      
-      post '/up-vote' => 'votes#up_vote', as: :up_vote
-      post '/down-vote' => 'votes#down_vote', as: :down_vote
-    end
+    resources :posts, except: [:index]
   end
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+
+  #post 'upvote', on: :member
+  #post 'downvote', on: :member
+  
+  post '/up-vote' => 'votes#up_vote', as: :up_vote
+  post '/down-vote' => 'votes#down_vote', as: :down_vote
   
   get 'about' => 'welcome#about'
 
