e73389156cd932981d167bdefbc901b98318d578
Stephen Lott <stephenwlott@gmail.com>
2015-04-14 04:12:53 +0000

    popular posts lesson complete

---
+++ app/controllers/topics/posts_controller.rb
@@ -0,0 +1,71 @@
+class Topics::PostsController < ApplicationController
+  def show
+    @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
+    @comment = Comment.all
+    authorize @topic
+  end
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new
+    authorize @post
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = current_user.posts.build(post_params)
+    @post.topic = @topic
+    
+    authorize @post
+    if @post.save_with_initial_vote
+      flash[:notice] = "Post was saved."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+  
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+  end
+  
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @post = current_user.posts.build(post_params)
+    @post.topic = @topic
+    authorize @post
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      @post.reload
+      flash[:notice] = "Post was updated."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+  
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    title = @post.title
+    authorize @post
+ 
+    if @post.destroy
+      flash[:notice] = "\"#{title}\" was deleted successfully."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error deleting the post."
+      render :show
+    end
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
